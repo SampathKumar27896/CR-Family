5fe1ec03a7fc44eaca15497f17d86a65a4b8b0a1
Sampath Kumar <sampath.nagarajan@heptagon.in>
2020-05-19 12:35:31 +0530

    removed the code

--- controller/player.js
+++ controller/player.js
@@ -7,18 +7,18 @@ const player_info_url = constant.api.player_info_url;
 class Player {
 
     getPlayerInfo(req, res) {
-        let options = []; 
-        PLAYER_IDS.forEach((item, index) => {
-            options[index] = Common.getGETMethodOptions(player_info_url, encodeURI(item));
-        });
-        Promise.all([Common.sendHTTPRequest(options[0]),Common.sendHTTPRequest(options[1]),Common.sendHTTPRequest(options[2])])
-        .then(results => {
-            res.render('index',{player_info : this.setPlayerInfo(results)})
-        })
-        .catch(error => {
-            console.log(error);
-        })
-        
+        // let options = []; 
+        // PLAYER_IDS.forEach((item, index) => {
+        //     options[index] = Common.getGETMethodOptions(player_info_url, encodeURI(item));
+        // });
+        // Promise.all([Common.sendHTTPRequest(options[0]),Common.sendHTTPRequest(options[1]),Common.sendHTTPRequest(options[2])])
+        // .then(results => {
+        //     res.render('index',{player_info : this.setPlayerInfo(results)})
+        // })
+        // .catch(error => {
+        //     console.log(error);
+        // })
+        res.render('index')
     }
 
     setPlayerInfo(results){
